d439184699a335560a759ab8fd1e3c94566143cb
Brian Knight <knight.j.brian@gmail.com>
2025-05-05 12:21:26 -0600

    completed #128 POST, PUT, & DELETE

--- src/main/java/knight/brian/restcruddemo/rest/EmployeeRestController.java
+++ src/main/java/knight/brian/restcruddemo/rest/EmployeeRestController.java
@@ -3,7 +3,12 @@ package knight.brian.restcruddemo.rest;
 import knight.brian.restcruddemo.entity.Employee;
 import knight.brian.restcruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +28,39 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null ) {
+            throw new RuntimeException("Employee ID: %d not found.".formatted(employeeId));
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee save(@RequestBody Employee employee) {
+        // set id to 0 to make sure we are saving a new employee
+        employee.setId(0);
+
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee update(@RequestBody Employee employee) {
+
+        Employee updatedEmployee = employeeService.save(employee);
+
+        return updatedEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void delete() {
+
     }
 
 }
--- src/main/java/knight/brian/restcruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/knight/brian/restcruddemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import knight.brian.restcruddemo.dao.EmployeeDAO;
 import knight.brian.restcruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,6 +22,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> findAll() {
 
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 
 }
